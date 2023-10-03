4d022f3aa6ed9208df484f93b77311a4475c4276
seocit <abdrosyiids@gmail.com>
2023-10-03 12:25:11 +0700

    Initial project

---
+++ bin/dart_application_if3b.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_if3b/dart_application_if3b.dart' as dart_application_if3b;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_if3b.calculate()}!');
+}
